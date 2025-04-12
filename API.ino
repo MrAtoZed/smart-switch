c3161f428e5941570ae33d03c8105b3b39e85e79
YovS <120002590+MrAtoZed@users.noreply.github.com>
2025-04-13 06:08:17 +0700

    Delete API.ino

--- API.ino
+++
@@ -1,4 +0,0 @@
-//API
-
-//tambah schedulling : http://192.168.22.31/set_time?coil=1&startHour=14&startMinute=48&endHour=14&endMinute=50
-//hapus scheduling : http://192.168.22.31/delete_time?coil=1&id=0 
